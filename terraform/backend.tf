85a16777308e1dfbbabd83f8ace70036ed756dee
Bohdan Kapliuk <bogdankuplyk33@gmail.com>
2026-03-23 16:44:04 +0200

    Update S3 bucket name in backend configuration

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@
 
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-spring-boot-app-bkpliuk33"
+    bucket         = "terraform-state-spring-boot-app-bkpliuk"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
